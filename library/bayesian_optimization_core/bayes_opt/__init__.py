a1e95bb8be6323a87dbbd61777bc1ece9ef08e5f
Marco De Carlo <marco.decarlo@cmcc.it>
2024-05-21 17:32:18 +0200

    multi FSS update for single obs

--- library/bayesian_optimization_core/bayes_opt/__init__.py
+++ library/bayesian_optimization_core/bayes_opt/__init__.py
@@ -1,13 +1,12 @@
 import sys
 
-sys.path.append('/work/asc/machine_learning/projects/iMagine/bayes_opt_workflow')
+sys.path.append('/work/cmcc/machine_learning/md31923/iMagine/bayes_opt_20240213_test')
 
 from library.bayesian_optimization_core.bayes_opt.bayesian_optimization import BayesianOptimization, Events
 from library.bayesian_optimization_core.bayes_opt.domain_reduction import SequentialDomainReductionTransformer
 from library.bayesian_optimization_core.bayes_opt.util import UtilityFunction
 from library.bayesian_optimization_core.bayes_opt.logger import ScreenLogger, JSONLogger
 from library.bayesian_optimization_core.bayes_opt.constraint import ConstraintModel
-from library.path import Path
 
 __all__ = [
     "BayesianOptimization",
