4ca68ce4292ec116b7761cc0fa2b9f13df2c500f
Marco De Carlo <marco.decarlo@cmcc.it>
2025-04-10 15:06:18 +0200

    Rimuove i .pyc e __pycache__ tracciati

--- launcher_file/launch.sh
+++ launcher_file/launch.sh
@@ -7,10 +7,10 @@
 #BSUB -e ./job.err
 #BSUB -R "rusage[mem=3G]"
 
-ROOT="/work/cmcc/machine_learning/md31923/iMagine/bayes_opt_20240213_test"
+ROOT="/work/cmcc/machine_learning/md31923/iMagine/bayes_opt_mdk2"
 SIM="/work/cmcc/machine_learning/md31923/iMagine/simulation/Medslik_II"
 # SIM="/work/cmcc/machine_learning/md31923/iMagine"
-OBS="/work/cmcc/machine_learning/md31923/iMagine/bayes_opt_20240213_test/use_case_observations/syria/observations_2021_08_23_1000/20210824-0810-SYR-PL-A-01-S3"
+OBS="/work/cmcc/machine_learning/md31923/iMagine/bayes_opt_mdk2/use_case_observations/syria/observations_2021_08_23_1000/20210824-0810-SYR-PL-A-01-S3"
 # OBS="/work/cmcc/machine_learning/md31923/iMagine/bayes_opt_20240213_test/use_case_observations/cyprus/20220831-0343-CYP-UO-B-01-S1"
 # OBS="/work/cmcc/machine_learning/md31923/iMagine/bayes_opt_20240213_test/use_case_observations/palestine/20220413-1549-ISR-UO-B-01-S1"
 
